26af3aeb5547dfef0b9fd7344c4fd4d08a7cc42b
daon217 <daon217@gmail.com>
2025-09-16 10:19:09 +0900

    0901

--- shop/src/test/java/edu/sm/CSVApplicationTests.java
+++ shop/src/test/java/edu/sm/CSVApplicationTests.java
@@ -3,6 +3,7 @@ package edu.sm;
 import com.opencsv.CSVReader;
 import com.opencsv.exceptions.CsvValidationException;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.io.*;
@@ -10,9 +11,12 @@ import java.io.*;
 @SpringBootTest
 class CSVApplicationTests {
 
+    @Value("${app.dir.logsdirRead")
+    String dir;
+
     @Test
     void contextLoads() throws IOException, CsvValidationException {
-        CSVReader reader = new CSVReader(new InputStreamReader(new FileInputStream("C:\\smspring\\logs\\click.log"), "UTF-8"));
+        CSVReader reader = new CSVReader(new InputStreamReader(new FileInputStream(dir+"click.log"), "UTF-8"));
         String[] line;
 //        reader.readNext();  // 헤더 건너뜀
         StringBuffer sb = new StringBuffer();
