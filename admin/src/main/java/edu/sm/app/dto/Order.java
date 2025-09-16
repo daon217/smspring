67b56e6d08ca8afbb712ebba2292494953655cb0
daon217 <daon217@gmail.com>
2025-09-16 13:29:22 +0900

    0901

---
+++ admin/src/main/java/edu/sm/app/dto/Order.java
@@ -0,0 +1,19 @@
+package edu.sm.app.dto;
+
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Getter
+@Setter
+@Builder
+public class Order {
+    private int orderId;
+    private int productId;
+    private int cateId;
+    private LocalDateTime orderDate;
+    private int orderQt;
+}
